2404498bbd2517ade482ea504a7a3a128a49817b
Noa <noabkn05@gmail.com>
2024-05-26 10:20:14 +0300

    created driver

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Nashviel {
+    class Program
+    {
+        public static CONSTS myConsts = new CONSTS();
+
+        public static void Main()
+        {
+            string inputFilePath = $"{myConsts.CURR_FOLDER_NAME}\\{myConsts.CURR_INPUT_FILE_NAME}";
+            using FileStream openStream = File.OpenRead(inputFilePath);
+            Example? currExample = 
+                JsonSerializer.Deserialize<Example>(openStream);           
+            Utils.setID(0, currExample.addresses);
+            Utils.jsonfileGenerator(currExample);
+        }
+    }
+}
