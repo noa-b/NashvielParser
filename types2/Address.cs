51c66989cdf3569725a594974409b40ae4babda8
Noa <noabkn05@gmail.com>
2024-05-26 10:18:59 +0300

    created simpler test types for the input and output files

---
+++ types2/Address.cs
@@ -0,0 +1,7 @@
+namespace Nashviel{
+    class Address {
+        public string id { get; set; }
+        public string CITY { get; set; }
+        public string ZIP { get; set; }
+    }
+}
---
+++ types2/Example.cs
@@ -0,0 +1,7 @@
+namespace Nashviel{
+    class Example {
+        public string name { get; set; }
+        public string age { get; set; }
+        public Address[] addresses { get; set; }
+    }
+}
