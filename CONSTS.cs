a36cdef387d8110f4a44c0d5138ee71359f998b4
Noa <noabkn05@gmail.com>
2024-05-26 10:22:03 +0300

    created support class and files

---
+++ CONSTS.cs
@@ -0,0 +1,12 @@
+namespace Nashviel {
+    class CONSTS {
+        public string JSON_OUTPUT_EXTENTION = ".json";
+        public string METADATA_OUTPUT_EXTENTION = ".metadata";
+        public string[] NASHVILE_EXTENTION = {"json", "njson"};
+        public string CURR_FOLDER_NAME = "njson";
+        public string CURR_BASE_PATH = "D:\\Projects\\A\\C#\\NashvielParser";
+        public string CURR_INPUT_FILE_NAME = "myjson.json";
+        public string OUTPUT_DIRECTORY_NAME = "_parseJsons";
+        public string TEST_RECOURCES = "D:\\Projects\\A\\C#\\NashvielParser\\tests\\recources";
+    }
+}
